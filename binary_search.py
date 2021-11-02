88aaf1d2f7596bde3efb0472128fca19714219bd
PhuePyaeMaung <phuepyaemaung2000@gmail.com>
2021-11-02 15:32:10 +0630

    Update binary_search.py

--- binary_search.py
+++ binary_search.py
@@ -1,24 +1,29 @@
 def binary_search(my_list,item):
   '''
-  my_list: the input list
-  item: the number to be searched
-  Takes in my_list and item as parameters
-  If the item is found, returns the index number
-  If the item is not found, return None
+  Search item in the sorted list.
+  INPUT:
+  my_list : list of items
+  item: item to be searched
+  OUTPUT:
+  mid : index of search item
   '''
+  my_list.sort()
   low = 0
   high = len(my_list)-1
   while low <= high:
     mid = round((low + high)/2)
-    name = my_list[mid]
-    if name == item:
+    guess = my_list[mid]
+    if guess == item:
       return mid
-    if name > item:
+    if guess > item:
       high = mid-1
     else:
       low = mid+1
   return None
 
-my_list = [2,4,6,8,10]
-print(binary_search(my_list, 6))
-print(binary_search(my_list, 7))
+my_list = [5,9,1,7,4]
+
+if __name__ == '__main__':
+  print(binary_search(my_list,9))
+
+
